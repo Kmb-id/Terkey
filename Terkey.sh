aa780d8bfa3574d4e2171a00bd061987a1fb0419
L4EROOR <44727800+Kmb-id@users.noreply.github.com>
2025-07-09 22:47:27 +0700

    Delete Terkey.sh

--- Terkey.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# _*_ coding: utf-8 _*_
-
-echo -e ("installing . . . ") 
-(cd ~/;mkdir .termux;cd .termux;curl https://raw.githubusercontent.com/Kmb-id/Terkey/refs/heads/main/termux.properties -o termux.properties;cd ~/;termux-reload-settings) &> /dev/null
-echo -e ("Sukses \n Please Restart your Termux") 
